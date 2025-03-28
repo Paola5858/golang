356d907c9382b182a75429fe4f20c7a2c01f27fb
Paola5858 <paolasesi351@email.com>
2025-03-28 08:42:58 -0300

    acesso

---
+++ aula2/acesso.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var usuario, senha string
+
+	fmt.Println("💖 Bem-vindo ao sistema mais glamouroso do universo! 🌟")
+	fmt.Print("🎀 Digite seu usuário: ")
+	fmt.Scan(&usuario)
+
+	fmt.Print("🎀 Agora, digite sua senha: ")
+	fmt.Scan(&senha)
+
+
+	if usuario == "admin" && senha == "1234" {
+		fmt.Println("\n💋✨ Acesso PERMITIDO, divo(a)! Você brilhou! 💖🎀✨")
+	} else {
+		fmt.Println("\n❌ Acesso NEGADO, meu bem! Tente novamente! 💅")
+	}
+
+	fmt.Println("💎💖 Obrigado por usar nosso sistema fabuloso! 😘✨")
+}
