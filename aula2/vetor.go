8262e746a9e62fb3a187acd875a0e3a652ac81cd
Paola5858 <paolasesi351@email.com>
2025-04-03 08:16:07 -0300

    Vetores

---
+++ aula2/vetor.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var ages [5]int{1, 2, 3, 4, 5}
+	nomes := [5]string{"Paola", "Major", "Fabiano", "Heitor", "Carol"}
+	fmt.Println(ages)
+	fmt.Println(nomes)
+	}
