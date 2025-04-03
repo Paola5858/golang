3052a2fe61002e33c43e2232c9d938963d8ad8e0
Paola5858 <paolasesi351@email.com>
2025-04-03 09:19:09 -0300

    Slice

--- aula2/vetor.go
+++ aula2/vetor.go
@@ -3,12 +3,38 @@ package main
 import "fmt"
 
 func main() {
-    var ages = [5]int{1, 2, 3, 4, 5}
-    nomes := [5]string{"Paola", "Major", "Fabiano", "Heitor", "Carol"}
+ 
+    ages := [5]int{1, 2, 3, 4, 5}
+    names := [5]string{"Paola", "Major", "Fabiano", "Heitor", "Carol"}
 
-    fmt.Println(ages)
-    fmt.Println(nomes)
+    fmt.Println("Ages: ", ages)
+    fmt.Println("Names: ", names)
 
-    nomes[0] = "Paola"
-    fmt.Println(nomes[0])
+    names[0] = "Paola"
+    fmt.Println("Updated Names[0]: ", names[0])
+
+    scores := []int{1, 2, 3, 4, 5}
+    fmt.Println("Scores: ", scores)
+
+    scores[0] = 10
+    fmt.Println("Updated Scores[0]: ", scores[0])
+    fmt.Println(scores, len(scores), cap(scores))
+
+    rangeOne := scores[:2]
+    fmt.Println("Range One: ", rangeOne)
+    rangeTwo := scores[1:]
+    fmt.Println("Range Two: ", rangeTwo)
+    rangeThree := scores[:]
+    fmt.Println("Range Three: ", rangeThree)
+
+    var superheroes = []string{"Batman", "Superman", "Flash"}
+    fmt.Println("Superheroes: ", superheroes)
+
+   
+    superheroes = append(superheroes, "Wonder Woman")
+    fmt.Println("Updated Superheroes: ", superheroes, len(superheroes), cap(superheroes))
+
+   
+    superheroes = append(superheroes[:2], superheroes[3:]...)
+    fmt.Println("Updated Superheroes: ", superheroes)
 }
