971c6836278e4aeed80c8749f9e424b704ca1bc3
Paola5858 <paolasesi351@email.com>
2025-05-15 13:15:22 -0300

    feat: refatora lógica do jogo para incluir colisões e gerenciamento de asteroides

--- aula2/operadores/operador-lógico.go
+++ aula2/operadores/operador-lógico.go
@@ -2,7 +2,8 @@ package main
 
 import "fmt"
 
-func main() {
+func logicalOperators() {
+	fmt.Println("Executando função logicalOperators")
 	fmt.Println("Operadores lógicos")
 	fmt.Println("Operador AND: ", true && false)
 	fmt.Println("Operador OR: ", true || false)
--- jogoasteroide/main.go
+++ jogoasteroide/main.go
@@ -1,32 +1,46 @@
+// jogo/main.go
 package main
 
 import (
-	"fmt"
+	"image/color"
+	"log"
+	"math/rand"
+	"time"
+
 	"github.com/hajimehoshi/ebiten/v2"
-    "github.com/hajimehoshi/ebiten/v2/ebitenutil"
-    "image"
-    "log"
-	"runtime"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 const (
 	screenWidth  = 640
 	screenHeight = 480
-	playerSpeed  = 3
+	playerSpeed  = 4
 	playerWidth  = 64
 	playerHeight = 64
 	asteroidWidth  = 64
 	asteroidHeight = 64
 	asteroidSpeed = 2
-	)
+	numAsteroids = 5
+)
+
+type Asteroid struct {
+	x, y float64
+	img  *ebiten.Image
+}
+
 type Game struct {
 	playerX, playerY float64
-	player *ebiten.Image
-	asteroid *ebiten.Image
-	asteroidX, asteroidY float64
-	asteroidSpeed float64
+	player           *ebiten.Image
+	asteroids        []Asteroid
+	asteroidImage    *ebiten.Image
+	gameOver         bool
 }
+
 func (g *Game) Update() error {
+	if g.gameOver {
+		return nil
+	}
+
 	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) && g.playerX > 0 {
 		g.playerX -= playerSpeed
 	}
@@ -39,15 +53,90 @@ func (g *Game) Update() error {
 	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) && g.playerY < screenHeight-playerHeight {
 		g.playerY += playerSpeed
 	}
+
+	for i := range g.asteroids {
+		g.asteroids[i].y += asteroidSpeed
+		if g.asteroids[i].y > screenHeight {
+			g.asteroids[i].y = -asteroidHeight
+			g.asteroids[i].x = float64(rand.Intn(screenWidth - asteroidWidth))
+		}
+		if checkCollision(g.playerX, g.playerY, g.asteroids[i].x, g.asteroids[i].y) {
+			g.gameOver = true
+		}
+	}
+
 	return nil
 }
 
-func Draw(g *Game, screen *ebiten.Image) {
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(g.playerX, g.playerY)
-	screen.DrawImage(g.player, op)
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(color.RGBA{0, 0, 0, 255})
 
+	if g.gameOver {
+		ebitenutil.DebugPrint(screen, "Game Over!")
+	return
+}
+
+// player (nave)
+op := &ebiten.DrawImageOptions{}
+op.GeoM.Scale(0.5, 0.5) // Reduz imagem pela metade (ajuste conforme necessário)
+op.GeoM.Translate(g.playerX, g.playerY)
+screen.DrawImage(g.player, op)
+
+op := &ebiten.DrawImageOptions{}
+op.GeoM.Translate(g.playerX, g.playerY)
+screen.DrawImage(g.player, op)
+
+for _, a := range g.asteroids {
 	op = &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(g.asteroidX, g.asteroidY)
-	screen.DrawImage(g.asteroid, op)
+	op.GeoM.Scale(0.5, 0.5) // mesma coisa aqui
+	op.GeoM.Translate(a.x, a.y)
+	screen.DrawImage(a.img, op)
+}
+
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+func checkCollision(px, py, ax, ay float64) bool {
+	return px < ax+asteroidWidth && px+playerWidth > ax && py < ay+asteroidHeight && py+playerHeight > ay
+}
+
+func loadImage(path string) *ebiten.Image {
+	img, _, err := ebitenutil.NewImageFromFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return img
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	playerImg := loadImage("nave.png")
+	asteroidImg := loadImage("asteroide.png")
+
+	asteroids := make([]Asteroid, numAsteroids)
+	for i := range asteroids {
+		asteroids[i] = Asteroid{
+			x: float64(rand.Intn(screenWidth - asteroidWidth)),
+			y: float64(rand.Intn(screenHeight - asteroidHeight)),
+			img: asteroidImg,
+		}
+	}
+
+	game := &Game{
+		playerX:        100,
+		playerY:        100,
+		player:         playerImg,
+		asteroids:      asteroids,
+		asteroidImage:  asteroidImg,
+	}
+
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Asteroides da Paola 💫")
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
 }
