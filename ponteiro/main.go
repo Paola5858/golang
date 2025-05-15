a44cda887b9b007718c42720affed14236a96d19
Paola5858 <paolasesi351@email.com>
2025-05-15 09:45:33 -0300

    feat: adiciona função mudar para manipulação de variáveis

--- ponteiro/main.go
+++ ponteiro/main.go
@@ -2,9 +2,20 @@ package main
 
 import "fmt"
 
+func mudar(x int) {
+	x = 20
+	fmt.Println("Valor de x dentro da função mudar:", x)
+	z := 30
+	fmt.Println("Valor de z dentro da função mudar:", z)
+}
+
 func main() {
 	var a int = 10
 	var b *int = &a
 	fmt.Println("Valor de b:", b)
 	*b = 20
+	fmt.Println("Valor de a:", a)
+
+	c := 30
+	fmt.Println("Valor de c:", c)
 }
